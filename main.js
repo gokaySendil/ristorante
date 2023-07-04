3c088525c07eaeeb44901aab9c5293d41d1de8ea
Gökay Şendil <gokaysendilw@outlook.com>
2023-07-04 23:09:01 +0300

    Gallery page added (Complete)
    Lightgallery js scripts working on mobile&desktop views.
    Gallery grid changes between 50em to 70em
    TODO: Change the images

--- main.js
+++ main.js
@@ -1,18 +1,52 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const navLinks = document.querySelectorAll(".anchor-nav");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
-     ? navToggle.setAttribute("aria-expanded", false)
-     : navToggle.setAttribute("aria-expanded", true);
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
-  if(primaryNav.hasAttribute("data-visible")){
+  if (primaryNav.hasAttribute("data-visible")) {
     navToggle.classList.add("active");
-    document.body.style.overflowY = 'hidden';
-  }else{
+    document.body.style.overflowY = "hidden";
+  } else {
     navToggle.classList.remove("active");
-    document.body.style.overflowY = 'visible';
+    document.body.style.overflowY = "visible";
   }
   primaryHeader.toggleAttribute("data-overlay");
 });
+const resetLinks = () => {
+  navLinks.forEach((element) => {
+    element.classList.remove("active-nav");
+  });
+};
+const activateLink = (link) => {
+  navLinks.forEach((element) => {
+    if (element.innerHTML === link) {
+      element.classList.add("active-nav");
+    }
+  });
+};
+const selectNavLink = () => {
+  var path = window.location.pathname;
+  // Get the current html file
+  var page = path.split("/").pop();
+  // Add active-nav class to current active page's navbar link
+  switch (page) {
+    case "about.html":
+      activateLink("About");
+      break;
+    case "gallery.html":
+      activateLink("Gallery");
+      break;
+    case "contact.html":
+      activateLink("Contact");
+      break;
+    case "index.html":
+      resetLinks();
+      break;
+  }
+};
+selectNavLink();
